c9b66c22c9d3a8811feb927e856a722b1b102cca
Vitthal Sai Kaul <vitthalsai2001@gmail.com>
2023-03-26 13:47:08 +0000

    Add post confirmation cognito

---
+++ aws/lambdas/cruddur-post-confimation.py
@@ -0,0 +1,44 @@
+import json
+import os
+import psycopg2
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    user_display_name = user["name"]
+    user_email = user["email"]
+    user_handle = user["preferred_username"]
+    user_cognito_id = user["sub"]
+        
+    try:
+        sql = f"""
+        INSERT INTO public.users (
+            display_name,
+            email, 
+            handle, 
+            cognito_user_id
+            ) 
+        VALUES(
+           '{user_display_name}',
+           '{user_email}',
+           '{user_handle}',
+           '{user_cognito_id}'
+            )
+        """ 
+            
+        print(sql)
+        conn = psycopg2.connect(os.getenv("CONNECTION_URL"))
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        print('Commit Done')
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+
+    return event
